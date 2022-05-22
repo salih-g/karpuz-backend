ec01ea4cabe48932ff6a187bdabe9490a8fa1909
Salih Gumus <sudanmerinosu@gmail.com>
2022-05-22 18:22:07 +0300

    [feature][Salih] add auth route

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const templateRoute = require('./template.route');
+const authRoute = require('./auth.route');
 
 const router = express.Router();
 
@@ -7,6 +8,10 @@ const defaultRoutes = [
 	{
 		path: '/template',
 		route: templateRoute,
+	},
+	{
+		path: '/auth',
+		route: authRoute,
 	},
 ];
 
