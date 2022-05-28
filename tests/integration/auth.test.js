07fba8a0356a0aaa942c87f50da1db4836a9ade0
Salih Gumus <sudanmerinosu@gmail.com>
2022-05-28 16:52:01 +0300

    [feature][Salih] add login integration tests

--- tests/integration/auth.test.js
+++ tests/integration/auth.test.js
@@ -63,12 +63,73 @@ describe('Auth routes', () => {
 		});
 	});
 	describe('POST /v1/auth/login', () => {
-		// let newUser;
-		// beforeEach(() => {
-		// 	newUser = {
-		// 		username: faker.name.findName().toLowerCase(),
-		// 		password: 'password1',
-		// 	};
-		// });
+		let newUser;
+		beforeEach(() => {
+			newUser = {
+				username: faker.name.findName().toLowerCase(),
+				password: 'password1',
+			};
+		});
+
+		test('should return 200 and login user if username and password match', async () => {
+			await insertUsers([userOne]);
+			const loginCredentials = {
+				username: userOne.username,
+				password: userOne.password,
+			};
+
+			const res = await request(app)
+				.post('/v1/auth/login')
+				.send(loginCredentials)
+				.expect(httpStatus.OK);
+
+			expect(res.body.user).toEqual({
+				_id: expect.anything(),
+				createdAt: expect.anything(),
+				updatedAt: expect.anything(),
+				password: expect.anything(),
+				username: userOne.username,
+				role: userOne.role,
+			});
+
+			expect(res.body.tokens).toEqual({
+				access: { token: expect.anything(), expires: expect.anything() },
+			});
+		});
+
+		test('should return 401 error if there are no users with that email', async () => {
+			const loginCredentials = {
+				username: userOne.username,
+				password: userOne.password,
+			};
+
+			const res = await request(app)
+				.post('/v1/auth/login')
+				.send(loginCredentials)
+				.expect(httpStatus.UNAUTHORIZED);
+
+			expect(res.body).toEqual({
+				code: httpStatus.UNAUTHORIZED,
+				message: 'Incorrect username or password',
+			});
+		});
+
+		test('should return 401 error if password is wrong', async () => {
+			await insertUsers([userOne]);
+			const loginCredentials = {
+				username: userOne.username,
+				password: 'wrongPassword1',
+			};
+
+			const res = await request(app)
+				.post('/v1/auth/login')
+				.send(loginCredentials)
+				.expect(httpStatus.UNAUTHORIZED);
+
+			expect(res.body).toEqual({
+				code: httpStatus.UNAUTHORIZED,
+				message: 'Incorrect username or password',
+			});
+		});
 	});
 });
