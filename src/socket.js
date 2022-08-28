db5175b37695ce509c8d40aea109b15bf99dba4a
M.Salih Gumus <gumusmuhammedsalih@gmail.com>
2022-08-28 16:33:33 +0300

    [feature][Salih] add fetch_all_posts ✨

--- src/socket.js
+++ src/socket.js
@@ -2,6 +2,10 @@ const io = require('socket.io')();
 
 io.on('connection', (socket) => {
 	console.log(`${socket.id} connected`);
+	socket.on('creating_post', () => {
+		console.log('fetch_all_posts');
+		socket.broadcast.emit('fetch_all_posts');
+	});
 });
 
 module.exports = { io };
